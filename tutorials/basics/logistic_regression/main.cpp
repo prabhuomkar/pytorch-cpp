f9b8c7615470eeb8e8d3563686bd8e2317ba01ef
Omkar Prabhu <prabhuomkar@pm.me>
2019-01-16 21:38:53 +0530

    Initial commit

---
+++ tutorials/basics/logistic_regression/main.cpp
@@ -0,0 +1,6 @@
+#include <torch/torch.h>
+#include <iostream>
+
+int main() {
+  std::cout << "Logistic Regression" << std::endl;
+}
---
+++ tutorials/basics/pytorch_basics/main.cpp
@@ -0,0 +1,6 @@
+#include <torch/torch.h>
+#include <iostream>
+
+int main() {
+  std::cout << "PyTorch Basics" << std::endl;
+}
