638f992a9fda4beb333910ca9a451b88dbbb5eac
prabhuomkar <prabhuomkar@yandex.com>
2019-01-18 09:59:12 +0530

    Basic Autograd Example 2

--- tutorials/basics/pytorch_basics/main.cpp
+++ tutorials/basics/pytorch_basics/main.cpp
@@ -26,4 +26,44 @@ int main() {
   std::cout << x.grad() << std::endl;  // x.grad() = 2
   std::cout << w.grad() << std::endl;  // w.grad() = 1
   std::cout << b.grad() << std::endl;  // b.grad() = 1
+
+  ///////////////////////////////////////////////////////////
+  //                BASIC AUTOGRAD EXAMPLE 2               //
+  ///////////////////////////////////////////////////////////
+
+  // Create Tensors of shapes
+  x = torch::randn({10, 3});
+  y = torch::randn({10, 2});
+
+
+  // Build a fully connected layer
+  auto linear = torch::nn::Linear(3,2);
+  std::cout << "w: " << linear->weight << std::endl;
+  std::cout << "b: " << linear->bias << std::endl;
+
+  // Build loss function and optimizer
+  auto criterion = torch::nn::MSELoss();
+  auto optimizer = torch::optim::SGD(linear->parameters, torch::optim::SGDOptions(0.01));
+
+  // Forward pass
+  auto pred = linear(x);
+
+  // Compute loss
+  auto loss = criterion(pred, y);
+  std::cout << "loss item: " << loss.item().toFloat() << std::endl;
+
+  // Backward pass
+  loss.backward();
+
+  // Print out the gradients
+  std::cout << "dL/dw: " << linear->weight.grad() << std::endl;
+  std::cout << "dL/db: " << linear->bias.grad() << std::endl;
+
+  // 1 step gradient descent
+  optimizer.step();
+
+  // Print out the loss after 1-step gradient descent
+  pred = linear(x);
+  loss = criterion(pred, y);
+  std::cout << "loss after 1 step optimization: " << loss.item().toFloat() << std::endl;
 }
